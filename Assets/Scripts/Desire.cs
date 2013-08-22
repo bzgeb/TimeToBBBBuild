ec2fb2c733890f2749e443042f418dd1283efb91
Bronson Zgeb <bronson.zgeb@gmail.com>
2013-08-21 22:46:12 -0400

    Added a character who moves towards food when hungry.

---
+++ Assets/Scripts/Desire.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using RAIN.Core;
+
+public class Desire : MonoBehaviour {
+    public string varName;
+    public float level;
+    public float dropRate;
+
+    RAINAgent ai;
+
+	// Use this for initialization
+	void Start () {
+        ai = GetComponent<RAINAgent>();
+        if ( ai != null ) {
+            ai.Agent.actionContext.SetContextItem<float>( varName, level );
+        }
+	}
+
+    void Update() {
+        level -= dropRate * Time.deltaTime;
+
+        if ( ai != null ) {
+            ai.Agent.actionContext.SetContextItem<float>( varName, level );
+        }
+    }
+}
