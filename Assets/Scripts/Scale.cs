a4e8ef2166033d9d6ef306a6d468c6e415f01ed3
Bronson Zgeb <bronson.zgeb@gmail.com>
2013-09-11 21:06:37 -0400

    Taking out some AI. Adding new audio.
    Removing some of the AI stuff, to focus on the procedural audio element.
    Adding some new audio objects.  Added my scale and pitch lookup tables.

---
+++ Assets/Scripts/Scale.cs
@@ -0,0 +1,34 @@
+﻿public enum ScaleEnum
+{
+    Major,
+    Minor,
+    MelodicMinorAscending,
+    MelodicMinorDescending,
+    PentatonicMajor,
+    Ionian,
+    Aeolian,
+    Dorian,
+    Phrygian,
+    Lydian,
+    Mixolydian,
+    Locrian
+};
+
+public static class Scale
+{
+    public static readonly int[] Major = { 2, 2, 1, 2, 2, 2, 1 };
+    public static readonly int[] Minor = { 2, 1, 2, 2, 1, 2, 2 };
+    public static readonly int[] MelodicMinorAscending = { 2, 1, 2, 2, 2, 2, 1 };
+    public static readonly int[] MelodicMinorDescending = { 2, 1, 2, 2, 1, 2, 2 };
+    public static readonly int[] PentatonicMajor = { 2, 2, 3, 2, 3 };
+    public static readonly int[] Ionian = { 2, 2, 1, 2, 2, 2, 1 };
+    public static readonly int[] Aeolian = { 2, 1, 2, 2, 1, 2, 2 };
+    public static readonly int[] Dorian = { 2, 1, 2, 2, 2, 1, 2 };
+    public static readonly int[] Phrygian = { 1, 2, 2, 2, 1, 2, 2 };
+    public static readonly int[] Lydian = { 2, 2, 2, 1, 2, 2, 1 };
+    public static readonly int[] Mixolydian = { 2, 2, 1, 2, 2, 1, 2 };
+    public static readonly int[] Locrian = { 1, 2, 2, 1, 2, 2, 2 };
+
+    public static ScaleEnum currentScale;
+    public static int[] noteArray;
+}
